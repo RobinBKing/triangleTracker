0ffa4ce5875cff7e89c8a93729b3e5b79b21d95e
Pablo Vera and Rob King <samiamspop@gmail.com>
2016-01-19 14:56:10 -0800

    complete html, interface, all answers work

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
-var triangleEvaluation = function(side1, side2, side3) {
+var notTriangle = function(side1, side2, side3) {
   //ny two sides equal to the third side
   //two sides less than the third side
   if(side1 + side2 <= side3){
-    return false;
+    return true;
   }
   else if(side2 + side3 <= side1) {
-    return false;
+    return true;
   }
   else if(side3 + side1 <= side2) {
-      return false;
+      return true;
   }
+  else {
+    return false;
+  }
+};
+var equalTriangle = function(side1, side2, side3) {
   // is an equilateral triangle
   if (side1 === side2 && side1 === side3) {
       return true;
   }
+  else {
+    return false;
+  }
+};
+
+var isoscelesTriangle = function(side1, side2, side3) {
   //two sides are equal
   if(side1 === side2 && (side1 + side2 > side3)) {
     return true;
@@ -24,8 +35,40 @@ var triangleEvaluation = function(side1, side2, side3) {
   if(side1 === side3 && (side1 + side3 > side2)) {
     return true;
   }
+  else {
+    return false;
+  }
+};
+
+var scaleneTriangle = function(side1, side2, side3) {
   //No sides are equal
   if ((side1 !== side2) && (side1 !== side3) && (side1 + side2 > side3)) {
       return true;
   }
 };
+
+//interface
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var notTriangleresult = notTriangle(side1, side2, side3);
+    var equalTriangleresult = equalTriangle(side1, side2, side3);
+    var isoscelesTriangleresult = isoscelesTriangle(side1, side2, side3);// $(".year").text(year);
+    var scaleneTriangleresult = scaleneTriangle(side1, side2, side3);// $(".year").text(year);
+
+    if (notTriangleresult) {                 // same as writing if (result === false)
+      $(".triangleType").text("not a triangle.");
+    } else if (equalTriangleresult) {                 // same as writing if (result === false)
+      $(".triangleType").text("an equalateral triangle.");
+    } else if (isoscelesTriangleresult) {                 // same as writing if (result === false)
+      $(".triangleType").text("an isosceles triangle.");
+    } else if (scaleneTriangleresult) {                 // same as writing if (result === false)
+    $(".triangleType").text("a scalene triangle.");
+  }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,40 +1,40 @@
 describe('triangleEvaluation', function() {
   //ny two sides equal to the third side
   it("is false for any two sides equal to the third side", function() {
-    expect(triangleEvaluation(10, 10, 20)).to.equal(false);
+    expect(notTriangle(10, 10, 20)).to.equal(true);
   });
   it("is false for any two sides equal to the third side", function() {
-    expect(triangleEvaluation(20, 10, 10)).to.equal(false);
+    expect(notTriangle(20, 10, 10)).to.equal(true);
   });
   it("is false for any two sides equal to the third side", function() {
-    expect(triangleEvaluation(10, 20, 10)).to.equal(false);
+    expect(notTriangle(10, 20, 10)).to.equal(true);
   });
   //two sides less than the third side
   it("is false for any two sides less than the third side", function() {
-    expect(triangleEvaluation(5, 5, 15)).to.equal(false);
+    expect(notTriangle(5, 5, 15)).to.equal(true);
   });
   it("is false for any two sides less than the third side", function() {
-    expect(triangleEvaluation(15, 5, 5)).to.equal(false);
+    expect(notTriangle(15, 5, 5)).to.equal(true);
   });
   it("is false for any two sides less than the third side", function() {
-    expect(triangleEvaluation(5, 15, 5)).to.equal(false);
+    expect(notTriangle(5, 15, 5)).to.equal(true);
   });
   // is an equilateral triangle
   it("is true if it is an equilateral triangle", function() {
-    expect(triangleEvaluation(10, 10, 10)).to.equal(true);
-  });
-  //two sides are equal
-  it("is true if two sides are equal", function() {
-    expect(triangleEvaluation(10, 10, 15)).to.equal(true);
-  });
-  it("is true if two sides are equal", function() {
-    expect(triangleEvaluation(15, 10, 10)).to.equal(true);
-  });
-  it("is true if two sides are equal", function() {
-    expect(triangleEvaluation(10, 15, 10)).to.equal(true);
+    expect(equalTriangle(10, 10, 10)).to.equal(true);
   });
   //Sides are equal
   it("is true if no sides are equal", function() {
-    expect(triangleEvaluation(10, 15, 20)).to.equal(true);
+    expect(scaleneTriangle(10, 15, 20)).to.equal(true);
+  });
+  //two sides are equal
+  it("is true if two sides are equal", function() {
+    expect(isoscelesTriangle(10, 10, 15)).to.equal(true);
+  });
+  it("is true if two sides are equal", function() {
+    expect(isoscelesTriangle(15, 10, 10)).to.equal(true);
+  });
+  it("is true if two sides are equal", function() {
+    expect(isoscelesTriangle(10, 15, 10)).to.equal(true);
   });
 });
