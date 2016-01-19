52e94e6ffed464fa2d1d4d644e8d9cd5770a5230
Pablo Vera and Rob King <samiamspop@gmail.com>
2016-01-19 14:08:17 -0800

    add all unit test

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
 var triangleEvaluation = function(side1, side2, side3) {
-  if(side1 + side2 === side3){
+  //ny two sides equal to the third side
+  //two sides less than the third side
+  if(side1 + side2 <= side3){
     return false;
   }
-  else if(side2 + side3 === side1) {
+  else if(side2 + side3 <= side1) {
     return false;
   }
-  else if(side3 + side1 === side2) {
+  else if(side3 + side1 <= side2) {
       return false;
   }
-  else if(side1 + side2 < side3) {
-      return false;
+  // is an equilateral triangle
+  if (side1 === side2 && side1 === side3) {
+      return true;
   }
-  else if(side2 + side3 < side1) {
-        return false;
+  //two sides are equal
+  if(side1 === side2 && (side1 + side2 > side3)) {
+    return true;
   }
-  else if(side1 + side3 < side2) {
-        return false;
+  if(side2 === side3 && (side2 + side3 > side1)) {
+    return true;
+  }
+  if(side1 === side3 && (side1 + side3 > side2)) {
+    return true;
+  }
+  //No sides are equal
+  if ((side1 !== side2) && (side1 !== side3) && (side1 + side2 > side3)) {
+      return true;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,5 @@
 describe('triangleEvaluation', function() {
+  //ny two sides equal to the third side
   it("is false for any two sides equal to the third side", function() {
     expect(triangleEvaluation(10, 10, 20)).to.equal(false);
   });
@@ -8,6 +9,7 @@ describe('triangleEvaluation', function() {
   it("is false for any two sides equal to the third side", function() {
     expect(triangleEvaluation(10, 20, 10)).to.equal(false);
   });
+  //two sides less than the third side
   it("is false for any two sides less than the third side", function() {
     expect(triangleEvaluation(5, 5, 15)).to.equal(false);
   });
@@ -16,5 +18,23 @@ describe('triangleEvaluation', function() {
   });
   it("is false for any two sides less than the third side", function() {
     expect(triangleEvaluation(5, 15, 5)).to.equal(false);
+  });
+  // is an equilateral triangle
+  it("is true if it is an equilateral triangle", function() {
+    expect(triangleEvaluation(10, 10, 10)).to.equal(true);
+  });
+  //two sides are equal
+  it("is true if two sides are equal", function() {
+    expect(triangleEvaluation(10, 10, 15)).to.equal(true);
+  });
+  it("is true if two sides are equal", function() {
+    expect(triangleEvaluation(15, 10, 10)).to.equal(true);
+  });
+  it("is true if two sides are equal", function() {
+    expect(triangleEvaluation(10, 15, 10)).to.equal(true);
+  });
+  //Sides are equal
+  it("is true if no sides are equal", function() {
+    expect(triangleEvaluation(10, 15, 20)).to.equal(true);
   });
 });
