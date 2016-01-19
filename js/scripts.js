30b290985348206a9dcda3b902999c1b9895acb9
Pablo Vera and Rob King <samiamspop@gmail.com>
2016-01-19 11:54:37 -0800

    add more unit tests

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,14 @@ var triangleEvaluation = function(side1, side2, side3) {
   }
   else if(side3 + side1 === side2) {
       return false;
-    }
+  }
+  else if(side1 + side2 < side3) {
+      return false;
+  }
+  else if(side2 + side3 < side1) {
+        return false;
+  }
+  else if(side1 + side3 < side2) {
+        return false;
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,20 @@
 describe('triangleEvaluation', function() {
-  it("is false for any two sides greater than or equal to the third side", function() {
+  it("is false for any two sides equal to the third side", function() {
     expect(triangleEvaluation(10, 10, 20)).to.equal(false);
   });
-  it("is false for any two sides greater than or equal to the third side", function() {
+  it("is false for any two sides equal to the third side", function() {
     expect(triangleEvaluation(20, 10, 10)).to.equal(false);
   });
-  it("is false for any two sides greater than or equal to the third side", function() {
+  it("is false for any two sides equal to the third side", function() {
     expect(triangleEvaluation(10, 20, 10)).to.equal(false);
+  });
+  it("is false for any two sides less than the third side", function() {
+    expect(triangleEvaluation(5, 5, 15)).to.equal(false);
+  });
+  it("is false for any two sides less than the third side", function() {
+    expect(triangleEvaluation(15, 5, 5)).to.equal(false);
+  });
+  it("is false for any two sides less than the third side", function() {
+    expect(triangleEvaluation(5, 15, 5)).to.equal(false);
   });
 });
